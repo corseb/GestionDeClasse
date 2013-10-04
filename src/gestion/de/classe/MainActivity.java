f9c476976c969c13ff35d912ee3c9679a2343752
corseb <correard.pe2@gmail.com>
2013-10-05 00:07:57 +0200

    Spinner fonctionnel

--- src/gestion/de/classe/MainActivity.java
+++ src/gestion/de/classe/MainActivity.java
@@ -17,50 +17,42 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	// array_names contains the pupil names
-	String[] array_CE1 = getResources().getStringArray(R.array.liste_eleves_CE1);
-	String[] array_CM1 = getResources().getStringArray(R.array.liste_eleves_CM1);
-	static String[] array_names;
+	String[] array_classes;
+	String[] array_CE1;
+	String[] array_CM1;
+	String[] array_names;
 	// array_names contains the pupil names that are present and have not been asked a question yet (="remaining pupils" list)
 	ArrayList<String> presentNameList;
+	// at the beginning (before attendance check and interrogation) array_current_names is array_names
 	ArrayList<String> nameList;
 	// tVpupilName is the TextView in which the pupil name is displayed
 	TextView tVpupilName;
 	TextView tVdebugInfo;
 	Spinner spinClasse;
-	ArrayAdapter<CharSequence> adapClasse;	
+   	// Create an ArrayAdapter using the string array and a default spinner layout
+	ArrayAdapter<CharSequence> adapClasse;
+	int nbPupils;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		fill_arrays_and_textViews(1);
+		init_Vars(0);
 		fill_spinner();
-		//spinClasse.setOnItemSelectedListener(this);
 		addListenerOnSpinnerItemSelection();
 	}
 
-	public void addListenerOnSpinnerItemSelection() {
-		spinClasse.setOnItemSelectedListener(new OnItemSelectedListener(){
-
-			@Override
-			public void onItemSelected(AdapterView parent, View view, int pos, long id) {
-				Toast.makeText(parent.getContext(), "Classe sélectionnée : " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
-				fill_array_names(pos);
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView parent) {
-				// TODO Auto-generated method stub		
-			}});
-	  }
-
-	public void fill_arrays_and_textViews(int index_classe){
-		fill_array_names(index_classe);
-		// at the beginning (before attendance check and interrogation) array_current_names is array_names
+	private void init_Vars(int index_Classe){
+		array_classes= this.getResources().getStringArray(R.array.liste_classes);
+		array_CE1= this.getResources().getStringArray(R.array.liste_eleves_CE1);
+		array_CM1= this.getResources().getStringArray(R.array.liste_eleves_CM1);
+		tVpupilName = (TextView) findViewById(R.id.tV_pupil_name);
+		tVdebugInfo = (TextView) findViewById(R.id.tV_debug_info);
+		fill_array_names(index_Classe);
 		nameList = new ArrayList<String>(Arrays.asList(array_names));
-    	// get the right TextView
-    	tVpupilName = (TextView) findViewById(R.id.tV_pupil_name);
-    	tVdebugInfo = (TextView) findViewById(R.id.tV_debug_info);
+		spinClasse = (Spinner) findViewById(R.id.spinnerClasse);
+		adapClasse = ArrayAdapter.createFromResource(this, 
+				R.array.liste_classes, android.R.layout.simple_spinner_item);
 	}
 	
 	public void fill_array_names(int index_classe){
@@ -71,10 +63,43 @@ public class MainActivity extends Activity {
 			array_names = array_CM1;
 		}
 	}
+
+	public void addListenerOnSpinnerItemSelection() {
+		spinClasse.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView parent, View view, int pos, long id) {
+				Toast.makeText(parent.getContext(), "Classe sélectionnée : " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+				empty_nameList();
+				fill_array_names(pos);
+				tVpupilName.setText(array_classes[pos]);
+			
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView parent) {
+			}});
+	  }
 	
+	public void refill_nameList(){
+		for (int i = 0; i < array_names.length; i++) {
+			nameList.add(array_names[i]);
+		}
+	}
+		
+	public void empty_nameList(){
+//		StringBuilder debugTxt = new StringBuilder();
+//		debugTxt.append("EmptyList - avant : ");
+//		debugTxt.append(nameList.size());
+//		debugTxt.append(" après : ");
+		for (; nameList.size()>0;) {
+			nameList.remove(0);
+		}
+//		debugTxt.append(nameList.size());
+//		tVdebugInfo.setText(debugTxt);
+	}
+		
 	public void fill_spinner(){
-		spinClasse= (Spinner) findViewById(R.id.spinnerClasse);
-    	// Create an ArrayAdapter using the string array and a default spinner layout
     	adapClasse = ArrayAdapter.createFromResource(this, 
     			R.array.liste_classes, android.R.layout.simple_spinner_item);
     	// Specify the layout to use when the list of choices appears
@@ -82,7 +107,6 @@ public class MainActivity extends Activity {
     	// Apply the adapter to the spinner
     	spinClasse.setAdapter(adapClasse);		
 	}
-	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,14 +116,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public void refill_nameList(){
-		for (int i = 0; i < array_names.length; i++) {
-			nameList.add(array_names[i]);
-		}
-	}
-	
 	public void selectPupil(View view) {
-		int nbPupils;
 		// if nameList is empty, then refill it !
 		if (nameList.isEmpty()) {refill_nameList();}	
 		nbPupils = nameList.size();
@@ -112,21 +129,5 @@ public class MainActivity extends Activity {
 		// set the correct text
 		tVdebugInfo.setText("Élèves restants = "+nameList.size()+" - élèves interrogés = "+(array_names.length-nameList.size()));
 	}
-
-	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-		// parent.getItemAtPosition(pos);
-		if (pos==0){
-			array_names = this.getResources().getStringArray(R.array.liste_eleves_CE1);
-			tVdebugInfo.setText("-> CE1");
-		} else {
-			array_names = this.getResources().getStringArray(R.array.liste_eleves_CM1);			
-			tVdebugInfo.setText("-> CM1");
-		}
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-    }
 	
 }
