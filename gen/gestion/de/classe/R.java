86b3eea7194cdd9c8d8998bcae48c9b90d9a1d1b
corseb <correard.pe2@gmail.com>
2013-10-04 21:55:41 +0200

    avec spinner

--- gen/gestion/de/classe/R.java
+++ gen/gestion/de/classe/R.java
@@ -30,10 +30,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int button1=0x7f090000;
-        public static final int tV_debug_info=0x7f090002;
-        public static final int tV_pupil_name=0x7f090001;
+        public static final int action_settings=0x7f090005;
+        public static final int button1=0x7f090002;
+        public static final int spinnerClasse=0x7f090004;
+        public static final int tV_debug_info=0x7f090001;
+        public static final int tV_pupil_name=0x7f090000;
+        public static final int textUserSettings=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,9 +46,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int tV1_text=0x7f060002;
-        public static final int tV2_text=0x7f060004;
-        public static final int texte_bouton=0x7f060003;
+        public static final int menu_settings=0x7f060002;
+        public static final int tV1_text=0x7f060003;
+        public static final int tV2_text=0x7f060005;
+        public static final int texte_bouton=0x7f060004;
     }
     public static final class style {
         /** 
--- src/gestion/de/classe/MainActivity.java
+++ src/gestion/de/classe/MainActivity.java
@@ -1,57 +1,107 @@
 package gestion.de.classe;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
-import gestion.de.classe.R;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	// array_names contains the pupil names
-	String[] array_names;
+	String[] array_CE1 = getResources().getStringArray(R.array.liste_eleves_CE1);
+	String[] array_CM1 = getResources().getStringArray(R.array.liste_eleves_CM1);
+	static String[] array_names;
 	// array_names contains the pupil names that are present and have not been asked a question yet (="remaining pupils" list)
+	ArrayList<String> presentNameList;
 	ArrayList<String> nameList;
 	// tVpupilName is the TextView in which the pupil name is displayed
 	TextView tVpupilName;
 	TextView tVdebugInfo;
+	Spinner spinClasse;
+	ArrayAdapter<CharSequence> adapClasse;	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-    	// fill array_names with the default pupil name list
-    	array_names = this.getResources().getStringArray(R.array.liste_eleves_CM1);
-    	// at the beginning (before attendance check and interrogation) array_current_names is array_names
+		fill_arrays_and_textViews(1);
+		fill_spinner();
+		//spinClasse.setOnItemSelectedListener(this);
+		addListenerOnSpinnerItemSelection();
+	}
+
+	public void addListenerOnSpinnerItemSelection() {
+		spinClasse.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView parent, View view, int pos, long id) {
+				Toast.makeText(parent.getContext(), "Classe sélectionnée : " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+				fill_array_names(pos);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView parent) {
+				// TODO Auto-generated method stub		
+			}});
+	  }
+
+	public void fill_arrays_and_textViews(int index_classe){
+		fill_array_names(index_classe);
+		// at the beginning (before attendance check and interrogation) array_current_names is array_names
 		nameList = new ArrayList<String>(Arrays.asList(array_names));
     	// get the right TextView
     	tVpupilName = (TextView) findViewById(R.id.tV_pupil_name);
     	tVdebugInfo = (TextView) findViewById(R.id.tV_debug_info);
 	}
-
+	
+	public void fill_array_names(int index_classe){
+		// fill array_names with the corresponding pupil name list (0-> CE1, 1 -> CM1)
+		if (index_classe==0) {
+			array_names = array_CE1;}
+		else {
+			array_names = array_CM1;
+		}
+	}
+	
+	public void fill_spinner(){
+		spinClasse= (Spinner) findViewById(R.id.spinnerClasse);
+    	// Create an ArrayAdapter using the string array and a default spinner layout
+    	adapClasse = ArrayAdapter.createFromResource(this, 
+    			R.array.liste_classes, android.R.layout.simple_spinner_item);
+    	// Specify the layout to use when the list of choices appears
+    	adapClasse.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    	// Apply the adapter to the spinner
+    	spinClasse.setAdapter(adapClasse);		
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+//        getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
+	}
+	
+	public void refill_nameList(){
+		for (int i = 0; i < array_names.length; i++) {
+			nameList.add(array_names[i]);
+		}
 	}
 	
 	public void selectPupil(View view) {
 		int nbPupils;
-		if (nameList.isEmpty()) {
-			// if nameList is empty, then refill it !
-			for (int i = 0; i < array_names.length; i++) {
-				nameList.add(array_names[i]);
-			}
-		} 
+		// if nameList is empty, then refill it !
+		if (nameList.isEmpty()) {refill_nameList();}	
 		nbPupils = nameList.size();
 		// get a random index in the list
 		int nPupil = new Random().nextInt(nbPupils);
@@ -62,5 +112,21 @@ public class MainActivity extends Activity {
 		// set the correct text
 		tVdebugInfo.setText("Élèves restants = "+nameList.size()+" - élèves interrogés = "+(array_names.length-nameList.size()));
 	}
+
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+		// parent.getItemAtPosition(pos);
+		if (pos==0){
+			array_names = this.getResources().getStringArray(R.array.liste_eleves_CE1);
+			tVdebugInfo.setText("-> CE1");
+		} else {
+			array_names = this.getResources().getStringArray(R.array.liste_eleves_CM1);			
+			tVdebugInfo.setText("-> CM1");
+		}
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 	
 }
